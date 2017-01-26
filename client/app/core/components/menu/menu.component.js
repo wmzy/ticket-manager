dee8b707bdbb14f1d3a349a2976e144c9896a36f
wmzy <1256573276@qq.com>
2017-01-26 10:01:40 +0800

    rerename

--- client/app/core/components/menu/menu.component.js
+++ client/app/core/components/menu/menu.component.js
@@ -1,8 +1,8 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, ChangeDetectionStrategy} from '@angular/core';
+import {Router} from '@angular/router';
 
 import template from './menu.template.html';
-import { AuthService } from '../../../auth';
+import {AuthService} from '../../../auth';
 
 @Component({
   selector: 'top-menu',
@@ -10,17 +10,17 @@ import { AuthService } from '../../../auth';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MenuComponent {
-  constructor(userService: AuthService, router: Router) {
-    this.userService = userService;
+  constructor(authService: AuthService, router: Router) {
+    this.authService = authService;
     this._router = router;
   }
 
   getLoggedIn() {
-    return this.userService.getLoggedIn();
+    return this.authService.getLoggedIn();
   }
 
   logout() {
-    this.userService.logout();
+    this.authService.logout();
     this._router.navigate(['']);
     return false;
   }
